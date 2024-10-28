e796b2d865d5eea3815aec67704046d5275a3e8e
soniasankar <70152945+soniasankar@users.noreply.github.com>
2024-10-28 18:13:55 +0530

    issue 1 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,7 +41,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
@@ -58,36 +57,14 @@ import { HistoryComponent } from './history/history.component';
     PrescriptionsComponent,
     HistoryComponent,
     FilterPipe
-=======
-
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
-import { AuthGuard } from './auth/auth.guard';
-import { FormsModule } from '@angular/forms';  // <-- Ensure FormsModule is imported here
-
-import { CmsInterceptor } from './auth/cms.interceptor';
-
-// BookappointmentrComponent should only be declared in PatientsModule
-@NgModule({
-  declarations: [
-    AppComponent,
->>>>>>> 46dc3b434510d5996686cf79b0608f24036e0b75
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-<<<<<<< HEAD
     FormsModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
     AppRoutingModule
-=======
-    FormsModule,  // Ensure FormsModule is imported
-    BrowserAnimationsModule,  // Required animations module for Toastr
-    ToastrModule.forRoot(),  // ToastrModule for notifications
-    AppRoutingModule  // Application routing module
->>>>>>> 46dc3b434510d5996686cf79b0608f24036e0b75
   ],
   providers: [
     AuthGuard,
